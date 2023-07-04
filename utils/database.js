137b288d2e5133462bbb2e9598269a1b0a906b29
Samuel Viloria <samuelviloriadd@gmail.com>
2023-07-04 17:51:41 -0500

    ssl add

--- utils/database.js
+++ utils/database.js
@@ -7,7 +7,8 @@ const db = new Sequelize({
     database: process.env.DB_DATABASE,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {ssl: { require: true, rejectUnauthorized: false}}
 })
 
 
